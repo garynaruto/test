b968e15712ca2e046d6d345bf440a80ae9c0f4ad
garynaruto <garynaruto@gmail.com>
2021-02-02 23:57:46 +0800

    FG

---
+++ FG/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'DonutAutoLabelChart.dart';
+import 'SimpleBarChart.dart';
+import 'StackedHorizontalBarChart.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo yp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 350,
+              child:SimpleBarChart.withSampleData(),
+            ),
+            Container(
+              height: 200,
+              width: 350,
+              child:StackedHorizontalBarChart.withSampleData(),
+            ),
+            Container(
+              height: 200,
+              width: 350,
+              child:DonutAutoLabelChart.withSampleData(),
+            ),
+          ]
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
